5689d4dc90ab0dfb08d61b637d71c62bcb1b19ac
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-04-07 22:19:17 +0300

    infra: Добавлен и внедрён OpenAPI

--- src/main/java/com/example/tech_task/controller/DataRecordController.java
+++ src/main/java/com/example/tech_task/controller/DataRecordController.java
@@ -1,8 +1,11 @@
 package com.example.tech_task.controller;
 
 import com.example.tech_task.dto.request.DataRecordRequest;
-import com.example.tech_task.model.DataRecord;
 import com.example.tech_task.service.DataService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Data API", description = "Маршрутизация данных между БД")
 public class DataRecordController {
 
     private final DataService dataService;
 
+    @Operation(summary = "Сохранить запись", description = "Распределяет данные между БД по алгоритму type % N")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Успешно сохранено"),
+            @ApiResponse(responseCode = "400", description = "Невалидные данные")
+    })
     @PostMapping("/data")
     public ResponseEntity<?> createDataRecord(@Valid @RequestBody DataRecordRequest request){
         dataService.save(request);
--- src/main/java/com/example/tech_task/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/tech_task/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.tech_task.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
@@ -27,6 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleAll(Exception ex) {
+        log.error(ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("error", "Внутренняя ошибка сервера"));
     }
