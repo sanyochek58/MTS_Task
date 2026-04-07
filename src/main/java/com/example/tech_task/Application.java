821a04436fe5c15c7777d3e44b135bc6f55f2a63
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-04-07 18:45:01 +0300

    feat: Настроено подключения к БД

--- src/main/java/com/example/tech_task/Application.java
+++ src/main/java/com/example/tech_task/Application.java
@@ -1,7 +1,9 @@
 package com.example.tech_task;
 
+import com.example.tech_task.config.DataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class Application {
